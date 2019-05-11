84d46533b031332336e99274c7f4d55ea2256cbe
johnny-keker <giomail.iv@gmail.com>
2019-05-12 00:31:34 +0300

    added message patterns for entering the cabe

--- ropeway.py
+++ ropeway.py
@@ -29,16 +29,26 @@ cabe = []
 def write_msg(user_id, s):
     vk.method('messages.send', {'user_id': user_id, 'message': s, 'random_id':random.randint(0, 100)})
 
+def add_user_to_cabe(u_id):
+    if u_id in cabe:
+        write_msg(u_id, already_registered(len(cabe), PEOPLE_AMOUNT))
+    else:
+        cabe.append(u_id)
+        if len(cabe) != PEOPLE_AMOUNT:
+            write_msg(u_id, on_registration(len(cabe), PEOPLE_AMOUNT))
+        else:
+            for uid in cabe:
+                write_msg(uid, messages["start"])
+            cabe.clear()
+
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        if event.user_id in cabe:
-            write_msg(event.user_id, already_registered(len(cabe), PEOPLE_AMOUNT))
-        else:
-            cabe.append(event.user_id)
-            if len(cabe) != PEOPLE_AMOUNT:
-                write_msg(event.user_id, on_registration(len(cabe), PEOPLE_AMOUNT))
-            else:
-                for u_id in cabe:
-                    write_msg(u_id, messages["start"])
-                cabe.clear()
+        valid_message = False
+        for pattern in messages["enter_patterns"]:
+            if pattern in event.text:
+                add_user_to_cabe(event.user_id)
+                valid_message = True
+                break
+        if not valid_message:
+            write_msg(event.user_id, messages["error"])
 
